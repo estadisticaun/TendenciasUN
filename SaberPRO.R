eb3a216d7e88978e0adc413bc47fee41f6780450
estadisticaun <estadistica_nal@unal.edu.co>
2018-07-19 16:52:21 -0500

    Actualización 19/07/2018

--- SaberPRO.R
+++ SaberPRO.R
@@ -3,6 +3,8 @@
 # LIBRERÍAS REQUERIDAS
 library(readxl)
 library(tidyverse)
+library(ggrepel)
+library(plotly)
 
 
 # IMPORTAR DATOS SABER PRO 2017
@@ -208,6 +210,8 @@ ggplot(data = SBPRO_2017_GEN, aes(y = MOD_INGLES_PUNT, x = fct_reorder(G15, MOD_
 ####################################################################
 
 
+# RAZONAMIENTO CUANTITATIVO
+
 QCuantitativo <- SBPRO_2017_GEN %>% 
   mutate(QRazCuant =  case_when(.$MOD_RAZONA_CUANTITATIVO_PNAL <= 20  ~ "Quintil1",
                                 .$MOD_RAZONA_CUANTITATIVO_PNAL > 20 & .$MOD_RAZONA_CUANTITATIVO_PNAL <= 40 ~ "Quintil2",
@@ -236,154 +240,297 @@ Facultades <- read_excel("Datos/Facultades.xlsx")
 QCuantitativo <- left_join(QCuantitativo, Facultades, by = "ESTU_SNIES_PRGMACADEMICO")
 
 
+RC_Facul <- QCuantitativo %>%  group_by(SEDE, FACULTAD) %>% 
+            summarise(Quintil1 = sum(Quintil1), Quintil2 = sum(Quintil2),
+                      Quintil3 = sum(Quintil3), Quintil4 = sum(Quintil4),
+                      Quintil5 = sum(Quintil5)) %>% 
+            mutate(Total = Quintil1 + Quintil2 + Quintil3 + Quintil4  + Quintil5) %>% 
+            mutate(Quintil1P = round((Quintil1/Total)*100, 0),
+            Quintil2P = round((Quintil2/Total)*100, 0),
+            Quintil3P = round((Quintil3/Total)*100, 0),
+            Quintil4P = round((Quintil4/Total)*100, 0),
+            Quintil5P = round((Quintil5/Total)*100, 0))
+# Gráfico
 
-
-
-names(SBPRO_2017_GEN)
-
-
-
-names(QCuantitativo)
-
-str_trunc(fruit, 3) 
-
-str_replace(QCuantitativo$G15, "UN-", "") 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-names(SBPRO_2017_GEN)
+p <- ggplot(RC_Facul, aes(x = Quintil1P, y = Quintil5P , color = SEDE, label = FACULTAD)) + 
+      geom_point(size = 6) + xlim(0, 30) + ylim(0, 100)+
+      geom_text_repel(col = "black", size = 4, force = 5) +
+      geom_hline(yintercept = 50, col = "green", size = 2) +
+      geom_vline(xintercept = 20, col = "#e34a33", size = 2) +
+      annotate("text", x = 12, y = 75, label = "Escenario ideal", colour = "gray", size = 10) +
+      annotate("text", x = 12, y = 20, label = "Escenario aceptable", colour = "gray", size = 10) +
+      annotate("text", x = 27, y = 20, label = "Escenario negativo", colour = "gray", size = 10) +
+      annotate("text", x = 27, y = 75, label = "Escenario atípico", colour = "gray", size = 10) +
+      scale_colour_brewer(palette = "Set1") +
+      ylab("Proporción de estudiantes en el quintil 5 (%)  \n") +
+      xlab("\n Proporción de estudiantes en el quintil 1 (%)") +
+      theme(
+      axis.title.x = element_text(size=14, face="bold"),
+      axis.title.y = element_text(size=14, face="bold"),
+      axis.text = element_text(size = 12)
+      )
   
-  MOD_RAZONA_CUANTITATIVO_PNAL
-  
-  
-  mutate(G12 =  case_when(.$INST_COD_INSTITUCION == 1813  ~ "U. de los Andes",
-                          .$INST_COD_INSTITUCION == 1714  ~ "U. del Rosario",
-                          .$INST_COD_INSTITUCION == 1706  ~ "U. Externado",
-                          .$INST_COD_INSTITUCION == 1828  ~ "ICESI",
-                          .$INST_COD_INSTITUCION %in% c(1201, 1219, 1222, 1223, 9125) ~ "U. de Antioquia",
-                          .$INST_COD_INSTITUCION == 1712  ~ "EAFIT",
-                          .$INST_COD_INSTITUCION == 1203  ~ "U. del Valle",
-                          .$INST_COD_INSTITUCION %in% c(1701, 1702)  ~ "U. Javeriana",
-                          .$INST_COD_INSTITUCION == 1204  ~ "UIS",
-                          .$INST_COD_INSTITUCION == 1713  ~ "U. del Norte",
-                          .$INST_COD_INSTITUCION %in% c(1710, 1723, 1727, 1730)  ~ "U. Bolivariana",
-                          .$INST_COD_INSTITUCION %in% c(1101, 1102, 1103, 1104)  ~ "U. Nacional",
-                          TRUE ~ "Resto IES"))
-MOD_RAZONA_CUANTITATIVO_PNAL
+ggplotly(p)
+
+# COMPETENCIAS CIUDADANAS
+
+
+QCuantitativo <- SBPRO_2017_GEN %>% 
+  mutate(QRazCuant =  case_when(.$MOD_COMPETEN_CIUDADA_PNAL <= 20  ~ "Quintil1",
+                                .$MOD_COMPETEN_CIUDADA_PNAL > 20 & .$MOD_COMPETEN_CIUDADA_PNAL <= 40 ~ "Quintil2",
+                                .$MOD_COMPETEN_CIUDADA_PNAL > 40 & .$MOD_COMPETEN_CIUDADA_PNAL <= 60 ~ "Quintil3",
+                                .$MOD_COMPETEN_CIUDADA_PNAL > 60 & .$MOD_COMPETEN_CIUDADA_PNAL <= 80 ~ "Quintil4",
+                                TRUE ~ "Quintil5"
+  )) %>% 
+  select(G12,G15, ESTU_SNIES_PRGMACADEMICO, ESTU_PRGM_ACADEMICO, QRazCuant) %>% 
+  filter(G12 == "U. Nacional") %>% 
+  group_by(G15, ESTU_SNIES_PRGMACADEMICO, ESTU_PRGM_ACADEMICO, QRazCuant) %>% 
+  summarise(Total = n()) %>% spread(QRazCuant, Total) %>% 
+  replace(., is.na(.), 0) %>% 
+  mutate(Total = Quintil1 + Quintil2 + Quintil3 + Quintil4  + Quintil5) %>% 
+  mutate(Quintil1P = round((Quintil1/Total)*100, 0),
+         Quintil2P = round((Quintil2/Total)*100, 0),
+         Quintil3P = round((Quintil3/Total)*100, 0),
+         Quintil4P = round((Quintil4/Total)*100, 0),
+         Quintil5P = round((Quintil5/Total)*100, 0)) %>% 
+  ungroup() %>% 
+  mutate(G15 = str_replace(G15, "UN-", ""))
+
+
+
+# Insertar Facultades
+
+Facultades <- read_excel("Datos/Facultades.xlsx")
+
+QCuantitativo <- left_join(QCuantitativo, Facultades, by = "ESTU_SNIES_PRGMACADEMICO")
+
+
+RC_Facul <- QCuantitativo %>%  group_by(SEDE, FACULTAD) %>% 
+  summarise(Quintil1 = sum(Quintil1), Quintil2 = sum(Quintil2),
+            Quintil3 = sum(Quintil3), Quintil4 = sum(Quintil4),
+            Quintil5 = sum(Quintil5)) %>% 
+  mutate(Total = Quintil1 + Quintil2 + Quintil3 + Quintil4  + Quintil5) %>% 
+  mutate(Quintil1P = round((Quintil1/Total)*100, 0),
+         Quintil2P = round((Quintil2/Total)*100, 0),
+         Quintil3P = round((Quintil3/Total)*100, 0),
+         Quintil4P = round((Quintil4/Total)*100, 0),
+         Quintil5P = round((Quintil5/Total)*100, 0))
+
+# Gráfico
+
+ggplot(RC_Facul, aes(x = Quintil1P, y = Quintil5P , color = SEDE, label = FACULTAD)) + 
+  geom_point(size = 6) + xlim(0, 30) + ylim(0, 100)+
+  geom_text_repel(col = "black", size = 4, force = 5) +
+  geom_hline(yintercept = 50, col = "green", size = 2) +
+  geom_vline(xintercept = 20, col = "#e34a33", size = 2) +
+  annotate("text", x = 12, y = 75, label = "Escenario ideal", colour = "gray", size = 10) +
+  annotate("text", x = 12, y = 20, label = "Escenario aceptable", colour = "gray", size = 10) +
+  annotate("text", x = 27, y = 20, label = "Escenario negativo", colour = "gray", size = 10) +
+  annotate("text", x = 27, y = 75, label = "Escenario atípico", colour = "gray", size = 10) +
+  scale_colour_brewer(palette = "Set1") +
+  ylab("Proporción de estudiantes en el quintil 5 (%)  \n") +
+  xlab("\n Proporción de estudiantes en el quintil 1 (%)") +
+  theme(
+    axis.title.x = element_text(size=14, face="bold"),
+    axis.title.y = element_text(size=14, face="bold"),
+    axis.text = element_text(size = 12)
+  )
+
+
+
+# LECTURA CRÍTICA
+
+
+QCuantitativo <- SBPRO_2017_GEN %>% 
+  mutate(QRazCuant =  case_when(.$MOD_LECTURA_CRITICA_PNAL <= 20  ~ "Quintil1",
+                                .$MOD_LECTURA_CRITICA_PNAL > 20 & .$MOD_LECTURA_CRITICA_PNAL <= 40 ~ "Quintil2",
+                                .$MOD_LECTURA_CRITICA_PNAL > 40 & .$MOD_LECTURA_CRITICA_PNAL <= 60 ~ "Quintil3",
+                                .$MOD_LECTURA_CRITICA_PNAL > 60 & .$MOD_LECTURA_CRITICA_PNAL <= 80 ~ "Quintil4",
+                                TRUE ~ "Quintil5"
+  )) %>% 
+  select(G12,G15, ESTU_SNIES_PRGMACADEMICO, ESTU_PRGM_ACADEMICO, QRazCuant) %>% 
+  filter(G12 == "U. Nacional") %>% 
+  group_by(G15, ESTU_SNIES_PRGMACADEMICO, ESTU_PRGM_ACADEMICO, QRazCuant) %>% 
+  summarise(Total = n()) %>% spread(QRazCuant, Total) %>% 
+  replace(., is.na(.), 0) %>% 
+  mutate(Total = Quintil1 + Quintil2 + Quintil3 + Quintil4  + Quintil5) %>% 
+  mutate(Quintil1P = round((Quintil1/Total)*100, 0),
+         Quintil2P = round((Quintil2/Total)*100, 0),
+         Quintil3P = round((Quintil3/Total)*100, 0),
+         Quintil4P = round((Quintil4/Total)*100, 0),
+         Quintil5P = round((Quintil5/Total)*100, 0)) %>% 
+  ungroup() %>% 
+  mutate(G15 = str_replace(G15, "UN-", ""))
+
+
+
+# Insertar Facultades
+
+Facultades <- read_excel("Datos/Facultades.xlsx")
+
+QCuantitativo <- left_join(QCuantitativo, Facultades, by = "ESTU_SNIES_PRGMACADEMICO")
+
+
+RC_Facul <- QCuantitativo %>%  group_by(SEDE, FACULTAD) %>% 
+  summarise(Quintil1 = sum(Quintil1), Quintil2 = sum(Quintil2),
+            Quintil3 = sum(Quintil3), Quintil4 = sum(Quintil4),
+            Quintil5 = sum(Quintil5)) %>% 
+  mutate(Total = Quintil1 + Quintil2 + Quintil3 + Quintil4  + Quintil5) %>% 
+  mutate(Quintil1P = round((Quintil1/Total)*100, 0),
+         Quintil2P = round((Quintil2/Total)*100, 0),
+         Quintil3P = round((Quintil3/Total)*100, 0),
+         Quintil4P = round((Quintil4/Total)*100, 0),
+         Quintil5P = round((Quintil5/Total)*100, 0))
+
+# Gráfico
+
+ggplot(RC_Facul, aes(x = Quintil1P, y = Quintil5P , color = SEDE, label = FACULTAD)) + 
+  geom_point(size = 6) + xlim(0, 30) + ylim(0, 100)+
+  geom_text_repel(col = "black", size = 4, force = 5) +
+  geom_hline(yintercept = 50, col = "green", size = 2) +
+  geom_vline(xintercept = 20, col = "#e34a33", size = 2) +
+  annotate("text", x = 12, y = 75, label = "Escenario ideal", colour = "gray", size = 10) +
+  annotate("text", x = 12, y = 20, label = "Escenario aceptable", colour = "gray", size = 10) +
+  annotate("text", x = 27, y = 20, label = "Escenario negativo", colour = "gray", size = 10) +
+  annotate("text", x = 27, y = 75, label = "Escenario atípico", colour = "gray", size = 10) +
+  scale_colour_brewer(palette = "Set1") +
+  ylab("Proporción de estudiantes en el quintil 5 (%)  \n") +
+  xlab("\n Proporción de estudiantes en el quintil 1 (%)") +
+  theme(
+    axis.title.x = element_text(size=14, face="bold"),
+    axis.title.y = element_text(size=14, face="bold"),
+    axis.text = element_text(size = 12)
+  )
 
 
 
 
+# COMUNICACIÓN ESCRITA
 
 
-SBPRO_2017_GEN1 <- SBPRO_2017_GEN %>% filter(!is.na(MOD_COMPETEN_CIUDADA_PUNT))
-
-ggplot(data = SBPRO_2017_GEN1, aes(y = MOD_COMPETEN_CIUDADA_PUNT, x = fct_reorder(G15, MOD_COMPETEN_CIUDADA_PUNT, fun = median(na.rm = TRUE)))) + 
-  geom_boxplot(outlier.color = "green", fill = "gray") +
-  ylim(0, 300) + geom_hline(yintercept = 180, col = "red", size = 1) +
-  geom_hline(yintercept = c(0, 300), col = "blue", size = 1, linetype="dashed") +
-  ylab("Puntaje Promedio Global SaberPRO 2017")+
-  xlab("Universidades")+coord_flip()+
-  theme(axis.text.y = element_text(size = 13, face = "bold"),
-        axis.title = element_text(face="bold", color="black", size=16)
-                )
-
-sum(is.na(SBPRO_2017_GEN$MOD_COMUNI_ESCRITA_PUNT))
-
-
-median(c(1,2,3, NA))
-
-###############################
-### Grupos de Referencia
-###############################
-
-# Distribución puntajes globales por área
-
-ggplot(data = SBPRO_2017_GEN, aes(x= GRUPOREFERENCIA, y = PUNT_GLOBAL)) + geom_boxplot(outlier.color = "green") +
-  ylim(0, 300) + geom_hline(yintercept = 146, col = "red", size = 1) + coord_flip()+
-  geom_hline(yintercept = c(0, 300), col = "blue", size = 1, linetype="dashed") 
-
-
-# Distribución puntajes globales por grupos de referencia
-
-ggplot(data = SBPRO_2017_GEN, aes(x= GRUPOREFERENCIA, y = PUNT_GLOBAL, fill = Unal)) + geom_boxplot(outlier.color = "green") +
-  ylim(0, 300) + geom_hline(yintercept = 146, col = "red", size = 1) + coord_flip()+
-  geom_hline(yintercept = c(0, 300), col = "blue", size = 1, linetype="dashed") 
-
-
-ggplot(data = SBPRO_2017_GEN, aes(x= Unal, y = PUNT_GLOBAL)) + geom_boxplot(outlier.color = "green") +
-  ylim(0, 300) + geom_hline(yintercept = 146, col = "red", size = 1) +
-  geom_hline(yintercept = c(0, 300), col = "blue", size = 1, linetype="dashed") 
-
-
-table(SBPRO_2017_GEN$Unal)
+QCuantitativo <- SBPRO_2017_GEN %>% 
+  mutate(QRazCuant =  case_when(.$MOD_COMUNI_ESCRITA_PNAL <= 20  ~ "Quintil1",
+                                .$MOD_COMUNI_ESCRITA_PNAL > 20 & .$MOD_COMUNI_ESCRITA_PNAL <= 40 ~ "Quintil2",
+                                .$MOD_COMUNI_ESCRITA_PNAL > 40 & .$MOD_COMUNI_ESCRITA_PNAL <= 60 ~ "Quintil3",
+                                .$MOD_COMUNI_ESCRITA_PNAL > 60 & .$MOD_COMUNI_ESCRITA_PNAL <= 80 ~ "Quintil4",
+                                TRUE ~ "Quintil5"
+  )) %>% 
+  select(G12,G15, ESTU_SNIES_PRGMACADEMICO, ESTU_PRGM_ACADEMICO, QRazCuant) %>% 
+  filter(G12 == "U. Nacional") %>% 
+  group_by(G15, ESTU_SNIES_PRGMACADEMICO, ESTU_PRGM_ACADEMICO, QRazCuant) %>% 
+  summarise(Total = n()) %>% spread(QRazCuant, Total) %>% 
+  replace(., is.na(.), 0) %>% 
+  mutate(Total = Quintil1 + Quintil2 + Quintil3 + Quintil4  + Quintil5) %>% 
+  mutate(Quintil1P = round((Quintil1/Total)*100, 0),
+         Quintil2P = round((Quintil2/Total)*100, 0),
+         Quintil3P = round((Quintil3/Total)*100, 0),
+         Quintil4P = round((Quintil4/Total)*100, 0),
+         Quintil5P = round((Quintil5/Total)*100, 0)) %>% 
+  ungroup() %>% 
+  mutate(G15 = str_replace(G15, "UN-", ""))
 
 
 
+# Insertar Facultades
 
-# SNIES	Sede
-# 1101	Bogotá
-# 1102	Medellín
-# 1103	Manizales
-# 1104	Palmira
-# 1124	Orinoquía
-# 1125	Amazonía
-# 1126	Caribe
-# 9920	Tumaco
+Facultades <- read_excel("Datos/Facultades.xlsx")
 
-G10
-
-# 1813 Universidad de los Andes
-# 1714 Universidad del Rosario
-# 1706 Universidad Externado
-# 1828 ICESI
-# 1201 Universidad de Antioquia
-# 1712 EAFIT
-# 1203 Universidad del Valle
-# 1701 1702 Universidad Javeriana
-# 1204 UIS
-# 1713 U del Norte
-# 1710 1723 1727 1730 U pont Bolivariana
-########  Unal
-# 1101	Bogotá
-# 1102	Medellín
-# 1103	Manizales
-# 1104	Palmira
-# 1124	Orinoquía
-# 1125	Amazonía
-# 1126	Caribe
-# 9920	Tumaco
+QCuantitativo <- left_join(QCuantitativo, Facultades, by = "ESTU_SNIES_PRGMACADEMICO")
 
 
-#######################################################
-################# ANÁLISIS POR PROGRAMAS ACADÉMICOS
-#######################################################
+RC_Facul <- QCuantitativo %>%  group_by(SEDE, FACULTAD) %>% 
+  summarise(Quintil1 = sum(Quintil1), Quintil2 = sum(Quintil2),
+            Quintil3 = sum(Quintil3), Quintil4 = sum(Quintil4),
+            Quintil5 = sum(Quintil5)) %>% 
+  mutate(Total = Quintil1 + Quintil2 + Quintil3 + Quintil4  + Quintil5) %>% 
+  mutate(Quintil1P = round((Quintil1/Total)*100, 0),
+         Quintil2P = round((Quintil2/Total)*100, 0),
+         Quintil3P = round((Quintil3/Total)*100, 0),
+         Quintil4P = round((Quintil4/Total)*100, 0),
+         Quintil5P = round((Quintil5/Total)*100, 0))
+
+# Gráfico
+
+ggplot(RC_Facul, aes(x = Quintil1P, y = Quintil5P , color = SEDE, label = FACULTAD)) + 
+  geom_point(size = 6) + xlim(0, 30) + ylim(0, 100)+
+  geom_hline(yintercept = 50, col = "green", size = 2) +
+  geom_vline(xintercept = 20, col = "#e34a33", size = 2) +
+  geom_text_repel(col = "black", size = 4, force = 5) +
+  annotate("text", x = 12, y = 75, label = "Escenario ideal", colour = "gray", size = 10) +
+  annotate("text", x = 12, y = 20, label = "Escenario aceptable", colour = "gray", size = 10) +
+  annotate("text", x = 27, y = 20, label = "Escenario negativo", colour = "gray", size = 10) +
+  annotate("text", x = 27, y = 75, label = "Escenario atípico", colour = "gray", size = 10) +
+  scale_colour_brewer(palette = "Set1") +
+  ylab("Proporción de estudiantes en el quintil 5 (%)  \n") +
+  xlab("\n Proporción de estudiantes en el quintil 1 (%)") +
+  theme(
+    axis.title.x = element_text(size=14, face="bold"),
+    axis.title.y = element_text(size=14, face="bold"),
+    axis.text = element_text(size = 12)
+  )
+
+# INGLÉS
+
+
+QCuantitativo <- SBPRO_2017_GEN %>% 
+  mutate(QRazCuant =  case_when(.$MOD_INGLES_PNAL <= 20  ~ "Quintil1",
+                                .$MOD_INGLES_PNAL > 20 & .$MOD_INGLES_PNAL <= 40 ~ "Quintil2",
+                                .$MOD_INGLES_PNAL > 40 & .$MOD_INGLES_PNAL <= 60 ~ "Quintil3",
+                                .$MOD_INGLES_PNAL > 60 & .$MOD_INGLES_PNAL <= 80 ~ "Quintil4",
+                                TRUE ~ "Quintil5"
+  )) %>% 
+  select(G12,G15, ESTU_SNIES_PRGMACADEMICO, ESTU_PRGM_ACADEMICO, QRazCuant) %>% 
+  filter(G12 == "U. Nacional") %>% 
+  group_by(G15, ESTU_SNIES_PRGMACADEMICO, ESTU_PRGM_ACADEMICO, QRazCuant) %>% 
+  summarise(Total = n()) %>% spread(QRazCuant, Total) %>% 
+  replace(., is.na(.), 0) %>% 
+  mutate(Total = Quintil1 + Quintil2 + Quintil3 + Quintil4  + Quintil5) %>% 
+  mutate(Quintil1P = round((Quintil1/Total)*100, 0),
+         Quintil2P = round((Quintil2/Total)*100, 0),
+         Quintil3P = round((Quintil3/Total)*100, 0),
+         Quintil4P = round((Quintil4/Total)*100, 0),
+         Quintil5P = round((Quintil5/Total)*100, 0)) %>% 
+  ungroup() %>% 
+  mutate(G15 = str_replace(G15, "UN-", ""))
 
 
 
-defaul_locale
+# Insertar Facultades
+
+Facultades <- read_excel("Datos/Facultades.xlsx")
+
+QCuantitativo <- left_join(QCuantitativo, Facultades, by = "ESTU_SNIES_PRGMACADEMICO")
 
 
-stocks <- tibble(
-  time = as.Date('2009-01-01') + 0:9,
-  X = rnorm(10, 0, 1),
-  Y = rnorm(10, 0, 2),
-  Z = rnorm(10, 0, 4)
-)
+RC_Facul <- QCuantitativo %>%  group_by(SEDE, FACULTAD) %>% 
+  summarise(Quintil1 = sum(Quintil1), Quintil2 = sum(Quintil2),
+            Quintil3 = sum(Quintil3), Quintil4 = sum(Quintil4),
+            Quintil5 = sum(Quintil5)) %>% 
+  mutate(Total = Quintil1 + Quintil2 + Quintil3 + Quintil4  + Quintil5) %>% 
+  mutate(Quintil1P = round((Quintil1/Total)*100, 0),
+         Quintil2P = round((Quintil2/Total)*100, 0),
+         Quintil3P = round((Quintil3/Total)*100, 0),
+         Quintil4P = round((Quintil4/Total)*100, 0),
+         Quintil5P = round((Quintil5/Total)*100, 0))
 
-gather(stocks, stock, price, -time)
-stocks %>% gather(stock, price, -time)
+# Gráfico
+
+ggplot(RC_Facul, aes(x = Quintil1P, y = Quintil5P , color = SEDE, label = FACULTAD)) + 
+  geom_point(size = 6) + xlim(0, 30) + ylim(0, 100)+
+  geom_hline(yintercept = 50, col = "green", size = 2) +
+  geom_vline(xintercept = 20, col = "#e34a33", size = 2) +
+  geom_text_repel(col = "black", size = 4, force = 5) +
+  annotate("text", x = 12, y = 75, label = "Escenario ideal", colour = "gray", size = 10) +
+  annotate("text", x = 12, y = 20, label = "Escenario aceptable", colour = "gray", size = 10) +
+  annotate("text", x = 27, y = 20, label = "Escenario negativo", colour = "gray", size = 10) +
+  annotate("text", x = 27, y = 75, label = "Escenario atípico", colour = "gray", size = 10) +
+  scale_colour_brewer(palette = "Set1") +
+  ylab("Proporción de estudiantes en el quintil 5 (%)  \n") +
+  xlab("\n Proporción de estudiantes en el quintil 1 (%)") +
+  theme(
+    axis.title.x = element_text(size=14, face="bold"),
+    axis.title.y = element_text(size=14, face="bold"),
+    axis.text = element_text(size = 12)
+  )
+
+
